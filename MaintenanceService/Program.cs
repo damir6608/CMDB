034da789d486d79258573bf10018c7dcc29a2675
dlatypov <latypovdamir3@gmail.com>
2023-06-20 06:51:49 +0300

    feat: politics

--- MaintenanceService/Program.cs
+++ MaintenanceService/Program.cs
@@ -1,5 +1,6 @@
 using MaintenanceService.Configuration;
 using MaintenanceService.Configuration.Interfaces;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+// If using IIS:
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
 
 var app = builder.Build();
 
